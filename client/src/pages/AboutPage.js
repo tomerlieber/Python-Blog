440ad2e9de4d239dcba0291b3c5bc18ef9963dfe
Tomer Lieber <tomerlieber@gmail.com>
2020-11-19 20:10:05 +0200

    Removed unnecessary 'and' word between attributes

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = () => (
         Hey, my name is Tomer.
         As a software developer with 4 years of experience, I advocate the principle of code reuse.
         So, instead of telling about myself here, I invite you to 
-        visit <a href="https://www.linkedin.com/in/tomer-lieber/" target="_blank" and rel="noopener noreferrer">my linkein profile</a> and know me even better :)
+        visit <a href="https://www.linkedin.com/in/tomer-lieber/" target="_blank" rel="noopener noreferrer">my linkein profile</a> and know me even better :)
     </p>
     </>
 );
