80025bfad5fc4adcee7f519fb09c1a2476db55c0
Tomer Lieber <tomerlieber@gmail.com>
2020-11-24 15:30:19 +0200

    Changed the content of the Home page text

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => (
             Welcome to my new blog about Python!
             The blog contains mostly posts about Python at a basic level. 
             It will help people who want to learn the language. 
-            However, even people who already know the language will be able to find helpful posts here.
+            However, even people with previous experience will be able to find helpful posts here.
             Feel free to comment on the posts and give a like if you liked what you read.
         </p>
     </div>
